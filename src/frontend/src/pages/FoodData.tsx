9e9d3016f7f5daded6363d004ca89e35c193ffd3
Maan Singh Rana <maanissmart@gmail.com>
2026-04-24 02:41:42 +0530

    added files

---
+++ src/frontend/src/pages/FoodData.tsx
@@ -0,0 +1,8 @@
+export const campusFood = [];
+export const foodRealityPoints = [];
+export const calculators = [];
+export const laptopExtras = [];
+export const laptopSpecs = [];
+export const nriDocuments = [];
+export const originalDocuments = [];
+export const packingGroups = [];
