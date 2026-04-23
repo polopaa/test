8287dd96ab03a28d320a7b647714f5876cb4af5f
Maan Singh Rana <maanissmart@gmail.com>
2026-04-24 02:48:08 +0530

    aaaaaaa

--- src/frontend/src/pages/FoodData.tsx
+++
@@ -1,8 +0,0 @@
-export const campusFood = [];
-export const foodRealityPoints = [];
-export const calculators = [];
-export const laptopExtras = [];
-export const laptopSpecs = [];
-export const nriDocuments = [];
-export const originalDocuments = [];
-export const packingGroups = [];
